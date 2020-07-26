fc3543da17f785670911f67c2e092bc934719dcd
iamvpa <iamvpa@users.noreply.github.com>
2020-07-26 23:56:59 +0530

    Escape

---
+++ lib/categories_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: <Widget>[],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
---
+++ lib/models/category.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final Color color;
+  Category({
+    @required this.id,
+    @required this.title,
+    this.color = Colors.orange,
+  });
+}
