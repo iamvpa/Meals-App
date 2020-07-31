12614ff10fca61bb252779d4c544a4df9c0e03c9
iamvpa <iamvpa@users.noreply.github.com>
2020-07-31 19:06:41 +0530

    c1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,14 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith()),
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotoCondensed',
+              ))),
       home: CategoriesScreen(),
     );
   }
