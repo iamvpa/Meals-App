41a9a9917b0f5517145a3fc8a223bbf41a4eb26c
iamvpa <iamvpa@users.noreply.github.com>
2020-07-31 13:06:08 +0530

    gg

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith()),
       home: CategoriesScreen(),
     );
   }
