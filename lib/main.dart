9ce69652f58a2be1fd8180e54e501f794bf245a4
iamvpa <iamvpa@users.noreply.github.com>
2020-08-01 20:15:16 +0530

    nu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/categories_screen.dart';
-import './category_meals_screen.dart';
+import 'package:flutter_complete_guide/screens/categories_screen.dart';
+import 'screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/category_meals_screen.dart';
+import 'package:flutter_complete_guide/screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
