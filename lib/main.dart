7ec0687dee012de92c40a18f1f5fd5140eb53edf
iamvpa <iamvpa@users.noreply.github.com>
2020-08-03 19:53:12 +0530

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,16 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
     );
   }
 }
