a740d5bd40b1e0d509044799277c525d62b4a754
iamvpa <iamvpa@users.noreply.github.com>
2020-08-04 18:55:38 +0530

    bb

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -26,6 +26,9 @@ class _TabsScreenState extends State<TabsScreen> {
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.category),
