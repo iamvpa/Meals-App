b0f1427d6313d63c48a4bb467a91806730082304
iamvpa <iamvpa@users.noreply.github.com>
2020-08-17 23:00:53 +0530

    performanceImprovement

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -4,9 +4,6 @@ class Category {
   final String id;
   final String title;
   final Color color;
-  const Category({
-    @required this.id,
-    @required this.title,
-    this.color = Colors.orange,
-  });
+  const Category(
+      {@required this.id, @required this.title, this.color = Colors.orange});
 }
